dca531f2133572248e6e0834d00b0012dc726884
Pockethouse <101947734+Pockethouse@users.noreply.github.com>
2024-12-03 13:45:38 -0500

    Polished the UI for a Sports, Events, and Events Market page

--- BetCity/wwwroot/js/site.js
+++ BetCity/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.body;
+    if (body.scrollHeight > window.innerHeight) {
+        body.classList.remove("no-scroll");
+    } else {
+        body.classList.add("no-scroll");
+    }
+});
